0972754a5549440241d02d6892ae00c9abc5431a
Vignesh Rajasekaran <vignesh@rvignesh.io>
2020-09-05 00:33:57 +0800

    Extract git configurations to separate file

--- README.md
+++ README.md
@@ -53,6 +53,10 @@ Following are the list of apps/tools which will be installed.
 
 ## Optional
 
+Following are optional scripts which can be run based on your need. I recommended reading through these scripts to understand what they do and what you need before running them.
+
+### Mac defaults
+
 You can also customise some Mac settings using the `defaults` command. I've set a few which are common for most of us. You can refered to this [repo1](https://github.com/kevinSuttle/macOS-Defaults/blob/master/.macos) & [repo2](https://github.com/mathiasbynens/dotfiles/blob/master/.macos) more a ton of other stuff that can be achieved.
 
 I've changed the following behaviours in OSX
@@ -61,3 +65,9 @@ I've changed the following behaviours in OSX
 - Show only active items in dock - I don't like clutter.
 - Increase keyrepeat and delay keyrepeat delay - I assume most devs like it this way.
 - Add bottom right corner as hot corner for locking screen
+
+The osx.sh file contains the commands for above. If you used the install script then go to `/opt/frog-mac-starter` to find the osx.sh file and run it there. 
+
+### Git defaults
+
+The git.sh script can help you to set some standard git defaults like you full name and email into the config. It also configures some aliases and creates a SSH key which can be used to authenticate yourselve.
--- osx.sh
+++ osx.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "==> 🔧 Setting OSX defaults"
 
 defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
@@ -15,39 +17,3 @@ echo "Set bottom right as hot corner... ✅"
 
 ln -s "~/Library/Mobile\ Documents/com\~apple\~CloudDocs" ~/icloud &> /dev/null
 echo "Symlink iCloud folder to ~/icloud...✅\n"
-
-echo "Configuring git..."
-
-printf "Enter your email: "
-read email
-
-printf "Enter your full name: "
-read full_name
-
-git config --global user.email "${email}"
-git config --global user.name "${full_name}"
-
-git config --global pull.rebase true
-git config --global core.autocrlf input
-git config --global alias.br branch
-git config --global alias.st status
-git config --global alias.co checkout
-git config --global alias.ci commit
-git config --global alias.unstage "reset HEAD --"
-git config --global alias.slog "log --pretty=oneline --abbrev-commit"
-git config --global alias.graph "log --all --oneline --graph --decorate"
-
-echo "Done configuring git ✅"
-
-if find ~/.ssh/id_rsa &> /dev/null
-then
-    echo "SSH key file already exists... ✅"
-else
-    ssh-keygen -t rsa -b 4096 -C "${email}" -q -N "" -f ~/.ssh/id_rsa
-    echo "Host *
-  AddKeysToAgent yes
-  UseKeychain yes
-  IdentityFile ~/.ssh/id_rsa" >> ~/.ssh/config
-    ssh-add -K ~/.ssh/id_rsa
-    echo "Done generating new SSH key file... ✅"
-fi
